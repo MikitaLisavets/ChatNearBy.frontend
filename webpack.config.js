dab25d93ee4988e7caeb05551d3b13cb6ea30a74
Mikita Lisavets <mikita.lisavets@gmail.com>
2017-03-08 21:12:43 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,185 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const DashboardPlugin = require('webpack-dashboard/plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const precss = require('precss');
+const autoprefixer = require('autoprefixer');
+
+const nodeEnv = process.env.NODE_ENV || 'development';
+const isProduction = nodeEnv === 'production';
+
+const jsSourcePath = path.join(__dirname, './src/scripts');
+const buildPath = path.join(__dirname, './dist');
+const assetsPath = path.join(__dirname, './src/assets');
+const sourcePath = path.join(__dirname, './src');
+
+// Common plugins
+const plugins = [
+  new webpack.optimize.CommonsChunkPlugin({
+    name: 'vendor',
+    minChunks: Infinity,
+    filename: 'vendor-[hash].js',
+  }),
+  new webpack.DefinePlugin({
+    'process.env': {
+      NODE_ENV: JSON.stringify(nodeEnv),
+    },
+  }),
+  new webpack.NamedModulesPlugin(),
+  new HtmlWebpackPlugin({
+    template: path.join(sourcePath, 'index.html'),
+    path: buildPath,
+    filename: 'index.html',
+  }),
+  new webpack.LoaderOptionsPlugin({
+    options: {
+      postcss: [
+        precss(),
+        autoprefixer({
+          browsers: [
+            'last 3 version',
+            'ie >= 10',
+          ],
+        }),
+      ],
+      context: sourcePath,
+    },
+  }),
+];
+
+// Common rules
+const rules = [
+  {
+    test: /\.(js|jsx)$/,
+    exclude: /node_modules/,
+    use: [
+      'babel-loader',
+    ],
+  },
+  {
+    test: /./,
+    include: assetsPath,
+    use: 'url-loader?limit=20480&name=assets/[name].[ext]',
+  },
+];
+
+if (isProduction) {
+  // Production plugins
+  plugins.push(
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false,
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+        screw_ie8: true,
+        conditionals: true,
+        unused: true,
+        comparisons: true,
+        sequences: true,
+        dead_code: true,
+        evaluate: true,
+        if_return: true,
+        join_vars: true,
+      },
+      output: {
+        comments: false,
+      },
+    }),
+    new ExtractTextPlugin('style-[hash].css')
+  );
+
+  // Production rules
+  rules.push(
+    {
+      test: /\.pcss$/,
+      loader: ExtractTextPlugin.extract({
+        use: [
+          'css-loader',
+          'postcss-loader',
+        ],
+      }),
+    }
+  );
+} else {
+  // Development plugins
+  plugins.push(
+    new webpack.HotModuleReplacementPlugin(),
+    new DashboardPlugin()
+  );
+
+  // Development rules
+  rules.push(
+    {
+      test: /\.pcss$/,
+      exclude: /node_modules/,
+      use: [
+        'style-loader?sourceMap',
+        'css-loader?sourceMap',
+        'postcss-loader?sourceMap',
+      ],
+    }
+  );
+}
+
+module.exports = {
+  devtool: isProduction ? 'eval' : 'source-map',
+  context: jsSourcePath,
+  entry: {
+    js: './index.js',
+    vendor: [
+      'babel-polyfill',
+      'es6-promise',
+      'immutable',
+      'isomorphic-fetch',
+      'react-dom',
+      'react-redux',
+      'react-router',
+      'react',
+      'redux-thunk',
+      'redux',
+    ],
+  },
+  output: {
+    path: buildPath,
+    publicPath: '/',
+    filename: 'app-[hash].js',
+  },
+  module: {
+    rules,
+  },
+  resolve: {
+    extensions: ['.webpack-loader.js', '.web-loader.js', '.loader.js', '.js', '.jsx'],
+    modules: [
+      path.resolve(__dirname, 'node_modules'),
+      jsSourcePath,
+    ],
+  },
+  plugins,
+  devServer: {
+    contentBase: isProduction ? './dist' : './src',
+    historyApiFallback: true,
+    port: 3000,
+    compress: isProduction,
+    inline: !isProduction,
+    hot: !isProduction,
+    host: '0.0.0.0',
+    stats: {
+      assets: true,
+      children: false,
+      chunks: false,
+      hash: false,
+      modules: false,
+      publicPath: false,
+      timings: true,
+      version: false,
+      warnings: true,
+      colors: {
+        green: '\u001b[32m',
+      },
+    },
+  },
+};
